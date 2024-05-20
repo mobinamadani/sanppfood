d546eaacbb4ce52dfb007c40f57a796eb56bbc59
unknown <teach.madani@gmail.com.com>
2024-05-19 20:45:50 -0700

    welcom blade refactur

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -40,50 +40,156 @@
     </section>
 </article>
 
-<article class="background-gray2 border-2 m-t-4">
+<article class="background-gray2 border-2 m-t-4 p-b-2">
     <section class="flex2 txt-right">
 
         <div>
             <img src="../img/اپلیکیشن-غذا-اپمون-1.jpg" alt="" class="img2 margin-imgs">
         </div>
 
-        <div class="m-l-8">
-            <p class=""><strong class="p-b-2 txt-black txt-bold2 font-size1">صاحب کسب و کار هستید؟</strong></p>
+        <div class="m-l-8 m-b-4">
+            <p class=""><strong class=" txt-black txt-bold2 font-size1">صاحب کسب و کار هستید؟</strong></p>
             <p class="p-t-3 font-size2 ">.با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید</p>
             <button class="btn-3 l txt-bold font-size3 ul txt-white m-l-3 b m-t-1 p-t-1 m-l-5"><a href="{{route('seller.register')}}" class="txt-white ">ثبت نام فروشندگان</a></button>
         </div>
     </section>
 </article>
 
-<article class="background-white border-4 m-t-5">
-    <section class="">
-        <div>
-            <p>sJFCNedrgo;ehraf'psphvnfa svxkajfnvgqeragu9reuoqirjjakfnmgnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn</p>
-        </div>
+<article class="background-white border-4 ">
+    <h5 class="txt-black font-size4 m-t-6 txt-right">اسنپ‌ فود در شهرهای ایران</h5>
+        <section class="d-flex txt-right m-t-5 m-l-11">
+            <article class="m-r-6">
+                <p>رشت</p>
+                <p>سنندج</p>
+                <p>آمل</p>
+                <p>پرند</p>
+                <p>نوشهر</p>
+                <p>گرمدره</p>
+                <p>چابهار</p>
+                <p>سنگر</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>اراک</p>
+                <p>بیرجند</p>
+                <p>پردیس</p>
+                <p>بروجرد</p>
+                <p>ملایر</p>
+                <p>چالوس</p>
+                <p>فومن</p>
+                <p>یزد</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>قم</p>
+                <p>زنجان</p>
+                <p>سمنان</p>
+                <p>هشتگرد</p>
+                <p>بوشهر</p>
+                <p>آستارا</p>
+                <p>تفرش</p>
+                <p>اهواز</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>تبریز</p>
+                <p>کرمانشاه</p>
+                <p>دزفول</p>
+                <p>آبادان</p>
+                <p>کیش</p>
+                <p>رودهن</p>
+                <p>دماوند</p>
+                <p>جیرفت</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>شیراز</p>
+                <p>ساری</p>
+                <p>کاشان</p>
+                <p>ورامین</p>
+                <p>لنگرود</p>
+                <p>خوی</p>
+                <p>زابل</p>
+                <p>قشم</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>اصفحان</p>
+                <p>گرگان</p>
+                <p>ایلام</p>
+                <p>ری</p>
+                <p>قوچان</p>
+                <p>سهند</p>
+                <p>درود</p>
+                <p>فسا</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>کرج</p>
+                <p>اردبیل</p>
+                <p>شاهرود</p>
+                <p>اندیشه</p>
+                <p>ساوه</p>
+                <p>قرچک</p>
+                <p>نور</p>
+                <p>داراب</p>
+            </article>
+
+
+            <article class="m-r-6">
+                <p>مشهد</p>
+                <p>کرمان</p>
+                <p>سبزوار</p>
+                <p>پاکدشت</p>
+                <p>صدرا</p>
+                <p>سیرجان</p>
+                <p>ملارد</p>
+                <p>رودسر</p>
+            </article>
+
+
+            <article class="m-r-6">
+               <p>تهران</p>
+               <p>همدان</p>
+               <p>بجنورد</p>
+               <p>مرودشت</p>
+               <p>اسکو</p>
+               <p>فولادشهر</p>
+               <p>رامسر</p>
+               <p>نکا</p>
+           </article>
     </section>
 </article>
 </body>
 
 <footer class="background-footer">
-    <article class="text-pink txt-right txt-bold2 font-size4 line-height m-b-2">
-        <p class="p-t-3 m-r-1">Snapp!</p>
-        <p class="p-b-3 m-r-1">Food</p>
+    <article class=" line-height m-b-2">
+        <p class="p-t-3 m-r-1 text-pink txt-right txt-bold2 font-size4">Snapp!</p>
+        <p class="p-b-3 m-r-1 text-pink txt-right txt-bold2 font-size4">Food</p>
+
+        <article class="txt-right">
+            <p class="text-pink txt-bold font-size4 m-r-3">اسنپ ‌فود</p>
+            <p class="m-r-3">تجربه سفارش غذا، از زودفود تا اسنپ ‌فود</p>
+            <section class="flex img3 m-r-4">
+                <img class="m-r-5" src={{asset("../img/Telegram_logo.svg.webp")}}>
+                <img class="m-r-5"  src={{asset("../img/Telegram_logo.svg.webp")}}>
+                <img class="m-r-5"  src={{asset("../img/Telegram_logo.svg.webp")}}>
+                <img class="m-r-5"  src={{asset("../img/Telegram_logo.svg.webp")}}>
+                <img class="m-r-5"  src={{asset("../img/Telegram_logo.svg.webp")}}>
+            </section>
+        </article>
+
     </article>
 
-    <article class="m-r-3 txt-right">
-        <p class="text-pink txt-bold font-size4">اسنپ ‌فود</p>
-        <p>تجربه سفارش غذا، از زودفود تا اسنپ ‌فود</p>
-        <section class="flex">
-            {{--                <img src="../img/icon_12">--}}
-            {{--                <img src="../img/icon_14">--}}
-            {{--                <img src="../img/icon_15">--}}
-            {{--                <img src="../img/images.jfif">--}}
-            {{--                <img src="../img/icon_12">--}}
-        </section>
-    </article>
 
-    <section class="d-flex">
-        <article class="m-l-10">
+    <section class="d-flex m-b-3 m-b-2">
+        <article class="m-l-10 p-b-1">
             <p>تماس با اسنپ ‌فود</p>
             <p>پرسش‌های متداول</p>
             <p>ثبت شکایات</p>
@@ -102,6 +208,5 @@
 
     </section>
 </footer>
-
 </html>
 
